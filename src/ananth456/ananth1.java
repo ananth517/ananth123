bccd6194c57842b212fb005ba09207d3858b80ac
Ananth Kumar M S <Ananth Kumar M S@DESKTOP-8N55IPS>
2019-12-25 12:15:15 +0530

    design

--- src/ananth456/ananth1.java
+++ src/ananth456/ananth1.java
@@ -1,5 +1,15 @@
 package ananth456;
 
-public class ananth1 {
+public class ananth1 {public ananth1() {
+	
 
+}
+public static void main(String [] args)
+{
+	System.out.println("1");
+	
+	
+	
+	
+}
 }
